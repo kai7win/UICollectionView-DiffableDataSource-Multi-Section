2ebebc95f3e0fe80d18231a49748c28234ebb678
Thomas <34007164+kai7win@users.noreply.github.com>
2024-05-02 15:50:12 +0800

    Initial Commit

---
+++ CollectionView diffable multi section/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView diffable multi section
+//
+//  Created by Thomas on 2024/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
